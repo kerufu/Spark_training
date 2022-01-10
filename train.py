46de1b52e2aad10ff5c990c22ef5dba36beb3053
keru <keru@lightfront.ai>
2022-01-09 23:43:03 -0800

    SVM

---
+++ train.py
@@ -0,0 +1,29 @@
+from pyspark.mllib.classification import SVMWithSGD, SVMModel
+from pyspark.mllib.regression import LabeledPoint
+from pyspark import SparkContext, SparkConf, AccumulatorParam
+
+conf = SparkConf()
+conf = conf.setAppName("sparkSVM")
+conf = conf.setMdaster("yarn")
+conf = conf.set("spark.submit.deployMode", "client")
+sc = SparkContext(conf=conf)
+
+# Load and parse the data
+def parsePoint(line):
+    values = [float(x) for x in line.split(' ')]
+    return LabeledPoint(values[0], values[1:])
+
+data = sc.textFile("$SPARK_HOME/data/mllib/sample_svm_data.txt")
+parsedData = data.map(parsePoint)
+
+# Build the model
+model = SVMWithSGD.train(parsedData, iterations=100)
+
+# Evaluating the model on training data
+labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
+trainErr = labelsAndPreds.filter(lambda lp: lp[0] != lp[1]).count() / float(parsedData.count())
+print("Training Error = " + str(trainErr))
+
+# Save and load model
+model.save(sc, "target/tmp/pythonSVMWithSGDModel")
+sameModel = SVMModel.load(sc, "target/tmp/pythonSVMWithSGDModel")
